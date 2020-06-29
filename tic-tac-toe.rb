2a719452cdc4e3aa003c041a3fe74579d64d2875
Vitor Souza <vitorhugops23@protonmail.com>
2020-06-29 13:10:59 -0300

    method game created, victory check pending

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,7 +1,3 @@
-module Helper
-
-end
-
 class Player
     attr_reader :type
 
@@ -17,8 +13,6 @@ end
 
 class Board
     @@position = Array.new(8)
-
-
     def self.show_table
         puts <<-HEREDOC
 
@@ -34,18 +28,48 @@ class Board
     def self.put_table(pos,player)
         @@position[pos] = player
     end
+
+    def self.victory?
+        
+    end
+
+    private
+
+    def self.vertical_checker
+
+    end
+
+    def self.horizontal_checker
+
+    end
+
+    def self.diagonal_checker
+
+    end
+end
+
+def game(player_one, player_two)
+    puts "Bem vindo ao jogo-da-velha!"
+    for i in 1..9 do
+        Board.show_table
+        puts ""
+        puts "Jogador1 - Escolha uma posição do tabuleiro: "
+        pos = gets.chomp.to_i
+        Board.put_table(pos, player_one)
+        puts "Feito!"
+        puts "Jogador2 - Escolha uma posição do tabuleiro: "
+        pos = gets.chomp.to_i
+        Board.put_table(pos, player_two)
+        puts "Aqui está o tabuleiro: "
+        #victory check
+
+    end
 end
 
 player1 = Player.new("X")
 player2 = Player.new("O")
 
-puts player1
-puts player2
-
-Board.put_table(0,"X")
-Board.show_table
-Board.put_table(0,"X")
-Board.show_table
+game(player1, player2)
 
 
 =begin
@@ -54,7 +78,17 @@ Think about how you would set up the different elements within the game… What 
 player deve ser uma classe
     o que deverá ter dentro dessa classe?
         - X ou O
-deverá ter um modulo para verificar a vitoria
 uma classe para mostrar o tabuleiro
+    deverá ter 3 metodos para verificar a vitoria
 um loop para rodar o jogo
+    ele devera perguntar ao p1 onde ele quer a peça dele
+    depois perguntar ao segundo jogador
+    mostrar o tabuleiro
+    checar a vitoria
+        se houver um vencedor
+            parar o jogo
+                mostrar uma mensagem de vitoria
+                    encerrar o programa
+
+COMENTARIO DO ALMOÇO: PRECISO FAZER O METODO VICTORY? LER AS CLASSES PRIVADAS SEM DAR ERRO
 =end
